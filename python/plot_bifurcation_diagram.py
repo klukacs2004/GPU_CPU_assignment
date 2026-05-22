930aab64539172fef1d96a13149c464cedf689a6
Krisztián József Lukács <82037880+klukacs2004@users.noreply.github.com>
2026-05-22 13:13:22 +0200

    Add differnet colorbar range for the gpu and cpu version in the python code

--- python/plot_bifurcation_diagram.py
+++ python/plot_bifurcation_diagram.py
@@ -31,8 +31,14 @@ def plot_bifurcation_diagram(filename, args):
         aspect="auto",
         cmap=cmap,
         extent=[args.r_min, args.r_max, args.x_min, args.x_max],
+
+        #range for the gpu version
         vmax=50000000,
         vmin=1000,
+
+        #for the cpu version
+        #vmax=3000000,
+        #vmin=10000,
         interpolation="bicubic",
     )
 
